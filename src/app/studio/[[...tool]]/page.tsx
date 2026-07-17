8245da4d450c27ee1094d08702b8fdbeb6188d9a
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2026-07-17 22:35:17 +0300

    perf(studio): optimize Sanity Studio route to force-static

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -3,7 +3,7 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-static";
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
